94bfa27012b0510bc2e41679980fe5d2586ae93a
Biiigstone <largestone23@gmail.com>
2025-08-11 18:23:23 +0900

    style: 주석 정리

--- src/run_4bit_q_t.py
+++ src/run_4bit_q_t.py
@@ -18,7 +18,7 @@ quantization_config = BitsAndBytesConfig(
     llm_int8_enable_fp32_cpu_offload=True
 )
 
-# 총 24개 레이어 중 15개(0~19)는 GPU에, 나머지(20~23)는 CPU로
+# 총 24개 레이어 중 20개(0~19)는 GPU에, 나머지(20~23)는 CPU로
 num_gpu_layers = 15
 num_total_layers = 24
 MAX_NEW_TOKEN = 1
@@ -49,22 +49,34 @@ tokenizer = AutoTokenizer.from_pretrained(model_path)
 
 print('토크나이저 로드 완료')
 
-
+# 대화 형식으로 질문 준비
 messages = [
     {"role": "user", "content": "Explain what MXFP4 quantization is in simple terms."},
 ]
 
+# 토크나이저로 모델이 이해할 수 있는 입력으로 변환
 inputs = tokenizer.apply_chat_template(
     messages,
     add_generation_prompt=True,
     return_tensors="pt"
-).to(model.device)
+).to(model.device) # 모델이 올라가 있는 장치(GPU/CPU)로 입력을 보냄
 
 
 gc.collect()
 torch.cuda.empty_cache()
+
 print('캐시 비우기 완료')
 
+# # 텍스트 생성 실행
+# # VRAM을 넘어선 부분은 RAM을 사용하므로 여기서 속도가 느려질 수 있습니다.
+# outputs = model.generate(**inputs, max_new_tokens=256)
+#
+# # 결과 출력
+# response_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+# # "assistant" 이후의 답변 부분만 깔끔하게 출력
+# print("\n--- 모델 답변 ---")
+# print(response_text.split("assistant\n")[-1])
+#
 messages = [
     {"role": "user", "content": "MXFP4 양자화에 대해 설명해줘."},
 ]
@@ -100,3 +112,19 @@ for i in range(10) :
 
 df = pd.DataFrame(execution_times, columns=['elapsed_time'])
 df.to_excel(f'./token_{MAX_NEW_TOKEN}_layer_{num_gpu_layers}.xlsx')
+
+
+# messages = [
+#     {"role": "system", "content": "Always respond in riddles"},
+#     {"role": "user", "content": "What is the weather like in Madrid?"},
+# ]
+#
+# inputs = tokenizer.apply_chat_template(
+#     messages,
+#     add_generation_prompt=True,
+#     return_tensors="pt",
+#     return_dict=True,
+# ).to(model.device)
+#
+# generated = model.generate(**inputs, max_new_tokens=500)
+# print(tokenizer.decode(generated[0][inputs["input_ids"].shape[-1]:]))
